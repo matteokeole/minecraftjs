d4dc1d2f186f85a241a60082a87bf21d361dd4a9
Mattéo <matteoo34@icloud.com>
2022-03-01 13:51:51 +0100

    Player bars

--- assets/js/eventlisteners.js
+++ assets/js/eventlisteners.js
@@ -24,4 +24,16 @@ addEventListener("resize", () => {
 	Renderer.setSize(window.innerWidth, window.innerHeight);
 	Camera.aspect = window.innerWidth / window.innerHeight;
 	Camera.updateProjectionMatrix()
-})
+});
+
+// Switch selected item (wheel event)
+addEventListener("wheel", e => {
+	if (e.deltaY > 0) {
+		// Right
+		selected_slot = (selected_slot < 8) ? selected_slot + 1 : 0;
+	} else {
+		// Left
+		selected_slot = (selected_slot > 0) ? selected_slot - 1 : 8;
+	}
+	inventory_bar_selector.updateX(inventory_bar_selector_slots[selected_slot]);
+});
--- assets/js/gui.js
+++ assets/js/gui.js
@@ -14,36 +14,39 @@ const
 			w: 0,
 			h: 0,
 		};
+		this.element = document.createElement("div");
+		this.element.style.cssText = `
+			width: ${w}px;
+			height: ${h}px;
+			margin: auto;
+			left: ${window.innerWidth / 2 - (w / 2) + x}px;
+			top: ${window.innerHeight / 2 - (h / 2) + y}px;
+			position: absolute;
+			background-color: transparent;
+			background-image: url(${tsrc});
+			background-position: ${-uv[0]}px ${-uv[1]}px;
+			background-repeat: no-repeat;
+			image-rendering: pixelated;
+			z-index: ${this.z};
+			transform: scale(4);
+		`;
+
+		this.updateX = function(x) {
+			this.element.style.left = `${window.innerWidth / 2 - (w / 2) + x}px`;
+		};
+
+		document.body.append(this.element);
 
 		// Load interface texture
 		const TEXTURE = new Image();
 		TEXTURE.addEventListener("load", () => {
 			this.texture.w = TEXTURE.width;
 			this.texture.h = TEXTURE.height;
-
-			// Create UI element
-			const element = document.createElement("div");
-			element.style.cssText = `
-				width: ${w}px;
-				height: ${h}px;
-				margin: auto;
-				left: ${window.innerWidth / 2 - (w / 2) + x}px;
-				top: ${window.innerHeight / 2 - (h / 2) + y}px;
-				position: absolute;
-				background-image: url(${tsrc});
-				background-position: ${-uv[0]}px ${-uv[1]}px;
-				background-repeat: no-repeat;
-				background-size: ${this.texture.w / 2}px;
-				image-rendering: pixelated;
-				z-index: ${this.z};
-				transform: scale(4);
-			`;
-
-			document.body.append(element);
+			this.element.style.backgroundSize = `${this.texture.w / 2}px`;
 		});
 		TEXTURE.src = this.texture.src;
 	},
-	crosshair = new UIElement([4.5, 4.5], [0, 0], ["assets/textures/gui/widgets.png", [121.5, 1.5], "crosshair"]),
+	crosshair = new UIElement([4.5, 4.5], [0, 0], ["assets/textures/gui/widgets.png", [121.5, 1.5]], "crosshair"),
 	inventory_bar = new UIElement([91, 11], [0, (window.innerHeight / 2 - (11 / 2) * 4)], ["assets/textures/gui/widgets.png", [0, 0]], "inventory_bar"),
 	inventory_bar_selector_slots = [
 		-159,
@@ -54,8 +57,47 @@ const
 		41,
 		81,
 		121,
-		141,
+		161,
 	],
-	inventory_bar_selector = new UIElement([12, 12], [inventory_bar_selector_slots[0], (window.innerHeight / 2 - (11 / 2) * 4 + 1), 1], ["assets/textures/gui/widgets.png", [0, 11]], "inventory_bar_selector");
+	hearth_slots = [
+		-173,
+		-157,
+		-141,
+		-125,
+		-109,
+		-93,
+		-77,
+		-61,
+		-45,
+		-29,
+	],
+	hunger_slots = [
+		29,
+		45,
+		61,
+		77,
+		93,
+		109,
+		125,
+		141,
+		157,
+		173,
+	],
+	inventory_bar_selector = new UIElement([12, 12], [inventory_bar_selector_slots[0], (window.innerHeight / 2 - (11 / 2) * 4 + 1), 1], ["assets/textures/gui/widgets.png", [0, 11]], "inventory_bar_selector"),
+	experience_bar = new UIElement([91, 2.5], [0, (window.innerHeight / 2 - (11 / 2) * 4 - 31)],  ["assets/textures/gui/icons.png", [0, 32]], "experience_bar");
+	health_bar = [],
+	hunger_bar = [];
 
-inventory_bar_selector.setPosition = 1;
+let selected_slot = 0;
+
+
+
+for (let i = 0; i < 10; i++) {
+	let heart_outline = new UIElement([4.5, 4.5], [hearth_slots[i], (window.innerHeight / 2 - (11 / 2) * 4 - 48)], ["assets/textures/gui/icons.png", [8, 0]], `heart-${i}`),
+		heart_inner = new UIElement([4, 3.5], [hearth_slots[i] + 1, (window.innerHeight / 2 - (11 / 2) * 4 - 48)], ["assets/textures/gui/icons.png", [26.5, .5]], `heart-${i}`);
+	health_bar.push([heart_outline, heart_inner]);
+
+	let hunger_outline = new UIElement([4.5, 4.5], [hunger_slots[i], (window.innerHeight / 2 - (11 / 2) * 4 - 48)], ["assets/textures/gui/icons.png", [8, 13.5]], `hunger-${i}`);
+		hunger_inner = new UIElement([3.5, 3.5], [hunger_slots[i], (window.innerHeight / 2 - (11 / 2) * 4 - 48)], ["assets/textures/gui/icons.png", [26.5, 14]], `hunger-${i}`);
+	health_bar.push([hunger_outline]);
+}
